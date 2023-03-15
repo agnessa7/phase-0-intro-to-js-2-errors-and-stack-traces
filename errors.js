21f3189fe1b122df916bedb5890e5163daff1168
Agnessa Nikolayev <anikolayev90@gmail.com>
2023-03-15 04:53:57 -0700

    Fixed erros, added comments

--- errors.js
+++ errors.js
@@ -5,7 +5,7 @@ const snackSelection = 'Pretzels';
 if (snackSelection === 'Pretzels') {
   console.log("That'll be $1, please!");
 }
-
+//changed "=" in the if statement to "==="
 
 // Once the code above this line is fixed you'll be able to see the error from the code below
 
@@ -20,4 +20,5 @@ function second () {
 function third() {
   console.log("Now I'm a function!");
 }
+//declared third() as a function
 first();
